59858c48fa3ac00434a2be0483e3a704cd78a36a
dungnguyen <dungnqhe160727@fpt.edu.vn>
2022-05-27 22:31:22 +0700

    Add important files

---
+++ show_maze.py
@@ -0,0 +1,26 @@
+import cv2
+from PIL import Image
+import numpy as np
+
+resolution = (1600, 800)
+img = np.array(Image.open('16 by 9 orthogonal maze.png').convert('RGB'))
+w, h, c = img.shape
+
+# cv2.namedWindow("Output")
+# cv2.moveWindow("Output", 40,30)  # Move it to (40,30)
+# cv2.resizeWindow("Output", resolution[0], resolution[1])
+
+new_w = int(w * 1.5)
+new_h = int(h * 1.5)
+
+# cv2.imshow('Original Image', img)
+# cv2.waitKey(0)
+# dim = (img.shape[1] * 5, img.shape[0] * 5)
+img = cv2.resize(img, (1600, 800), interpolation=cv2.INTER_CUBIC)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# img = cv2.copyMakeBorder(img, int((800 - new_h) / 2), int((800 - new_h) / 2), int((1600 - new_w) / 2),
+#                          int((1600 - new_h) / 2), 0)
+
+cv2.imshow('Resized Image', img)
+cv2.waitKey(0)
+# cv2.imwrite('dung.jpg', img)
