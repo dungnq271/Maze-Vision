278c69663ba757918fde293100d1225bc3dad6a8
dungnguyen <dungnqhe160727@fpt.edu.vn>
2022-05-29 02:53:54 +0700

    Update utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,22 +1,9 @@
-import cv2
-from PIL import Image
-import numpy as np
-
-
-def show_maze(img_path):
-    img = np.array(Image.open(img_path).convert('RGB'))
-
-    cv2.namedWindow('Resized Image', cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty('Resized Image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    cv2.imshow('Resized Image', img)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-# press any key to quit
-
-
-if __name__ == '__main__':
-    path = '../maze/16x9.png'
-    show_maze(path)
+def display_agent(img, agent, x, y, w, h):
+    img_w = img.shape[0]
+    img_h = img.shape[1]
+    # left = x - w // 2 if x - w // 2 >= 0 else w // 2 + x
+    # right = x + w // 2 if x + w // 2 <= img_w else img_w
+    # upper = y - h // 2 if y - h // 2 >= 0 else 0
+    # below = y + h // 2 if y + h // 2 <= img_h else img_h
+    img[y, w] = agent
+    return img
