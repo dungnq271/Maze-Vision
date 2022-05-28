dd9697ee8011a744ac217094a2db939e545c4ee9
dungnguyen <dungnqhe160727@fpt.edu.vn>
2022-05-28 12:20:16 +0700

    Show maze full screen

--- show_maze.py
+++ show_maze.py
@@ -11,7 +11,8 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 cv2.namedWindow('Resized Image', cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty('Resized Image', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 cv2.imshow('Resized Image', img)
-cv2.waitKey(2000)
+
+cv2.waitKey(0)
 cv2.destroyAllWindows()
 # press any key to quit
 
